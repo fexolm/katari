7d66c844131e7d0509666f9a24a7ba463956ca94
Alekseev Artem <a.artem060@gmail.com>
2018-09-08 21:02:07 +0300

    add simple interpreter

---
+++ src/ast.hpp
@@ -0,0 +1,43 @@
+//
+// Created by fexolm on 08.09.18.
+//
+
+#pragma once
+#include <memory>
+#include <vector>
+
+namespace ast {
+
+enum class types {
+  Int8,
+  Int16,
+  Int32,
+  Int64,
+  Uint8,
+  Uint16,
+  Uint32,
+  Uint64,
+  Float32,
+  Float64,
+  Ufloat32,
+  Ufloat64,
+  String,
+  Pointer,
+};
+
+struct base_node {
+  virtual ~base_node() = default;
+};
+
+template <typename T>
+struct value_node : base_node {
+  using value_type = T;
+  T value;
+  types type;
+};
+
+struct list_node : base_node{
+  std::vector<std::shared_ptr<base_node>> args;
+};
+
+}
---
+++ src/interpreter.cpp
@@ -0,0 +1,91 @@
+//
+// Created by fexolm on 08.09.18.
+//
+
+#include <memory>
+#include <iostream>
+#include <numeric>
+#include "ast.hpp"
+
+std::shared_ptr<ast::value_node<int>> read_value() {
+  auto res = std::make_shared<ast::value_node<int>>();
+  std::cin >> res->value;
+  return res;
+}
+
+std::shared_ptr<ast::value_node<std::string>> read_func() {
+  auto res = std::make_shared<ast::value_node<std::string>>();
+  std::cin >> res->value;
+  return res;
+}
+
+std::shared_ptr<ast::list_node> read_list() {
+  if (std::cin.get()!='(') return nullptr;
+
+  int count = 1;
+  std::shared_ptr<ast::list_node> res = std::make_shared<ast::list_node>();
+
+  while (count) {
+    while (std::cin.peek()==' ') {
+      std::cin.get();
+    }
+    switch (std::cin.peek()) {
+      case '(':;
+        res->args.push_back(read_list());
+        break;
+      case ')':count--;
+        std::cin.get();
+        break;
+      default:
+        if (std::isdigit(std::cin.peek())) {
+          res->args.push_back(read_value());
+        } else {
+          res->args.push_back(read_func());
+        }
+        break;
+    }
+  }
+  return res;
+}
+
+int do_op(std::string op, std::vector<int> args) {
+  if (op=="sum") {
+    return std::accumulate(args.begin(), args.end(), 0);
+  }
+  if (op=="mul") {
+    return std::accumulate(args.begin(), args.end(), 1, std::multiplies<int>());
+  }
+  return -100500;
+}
+
+int eval(std::shared_ptr<ast::list_node> root) {
+  if (!root) throw std::runtime_error("invalid code");
+  if (root->args.empty()) throw std::runtime_error("empty argument list");
+  auto op = std::dynamic_pointer_cast<ast::value_node<std::string>>(root->args[0]);
+  if (!op) throw std::runtime_error("no operation");
+
+  std::vector<int> args;
+
+  for (int i = 1; i < root->args.size(); i++) {
+    auto val_node = std::dynamic_pointer_cast<ast::value_node<int>>(root->args[i]);
+    if (val_node) {
+      args.push_back(val_node->value);
+      continue;
+    }
+    auto list_node = std::dynamic_pointer_cast<ast::list_node>(root->args[i]);
+    if (list_node) {
+      args.push_back(eval(list_node));
+      continue;
+    }
+    throw std::runtime_error("can not evaluate value");
+  }
+
+  return do_op(op->value, args);
+}
+
+int main() {
+  std::shared_ptr<ast::list_node> root = read_list();
+
+  std::cout << eval(root);
+
+}
